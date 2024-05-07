dc581cdb661419425f413e995502afd683d798e7
zhao-rui-NB <3457186@gmail.com>
2024-05-08 01:32:03 +0800

    analog first mbot_main

--- mbot_main/ir.h
+++ mbot_main/ir.h
@@ -1,117 +1,58 @@
 // #define IR_DEBUG
 
-
-const int IR_PIN[5] = {18, 16, 17, 14, 15};
+// 
+// int IR_BLACK[IR_CNT] = {930 , 908 , 901 , 902 , 931};
+// int IR_WHITE[IR_CNT] = { 32 ,  30 ,  29 ,  29 , 37 };
 
 
 const int IR_CNT = 5;
-int IR_BLACK[IR_CNT] = {930 , 908 , 901 , 902 , 931};
-int IR_WHITE[IR_CNT] = { 32 ,  30 ,  29 ,  29 , 37 };
-int IR_THRESHOLD[IR_CNT] = { 
-    (IR_BLACK[0]+IR_WHITE[0])/2,
-    (IR_BLACK[1]+IR_WHITE[1])/2,
-    (IR_BLACK[2]+IR_WHITE[2])/2,
-    (IR_BLACK[3]+IR_WHITE[3])/2,
-    (IR_BLACK[4]+IR_WHITE[4])/2
-};
-const int IR_WEIGHT[IR_CNT] = { -2, -1, 0, 1, 2 };
+const int IR_PIN[5] = {18, 16, 17, 14, 15};
+const float IR_WEIGHT[IR_CNT] = { -2, -1, 0, 1, 2 };
 
 
-bool ir_data[IR_CNT];
+// max is black , min is white
+u16 ir_max[5] = {0, 0, 0, 0, 0};
+u16 ir_min[5] = {1023, 1023, 1023, 1023, 1023};
+u16 ir_data[5] = {0, 0, 0, 0, 0};
+float ir_error = 0;
 
-
-float read_ir(){
-    static float last_res = 0;
-
-    // read ir sensors
-    // > threshold trigger , data = 1 
+float ir_read(bool update_range){
+    // save ir data and update range if needed
     for(int i=0 ; i<IR_CNT ; i++){
-        ir_data[i] = analogRead(IR_PIN[i]) > IR_THRESHOLD[i];
-    }
-
-    int res = 0;
-    if(ir_data[0])res =  -2;
-    else if(ir_data[4])res =   2;
-    else if(ir_data[1])res =   -1;
-    else if(ir_data[3])res =   1;
-    else if(ir_data[2])res =   0;
-    else if(last_res > 0){
-        res = 3;
-    }
-    else if(last_res < 0){
-        res = -3;
-    }
-    last_res = res;
-
-    return res;
-    
-
-}
-
-
-float read_ir__(){
-    static float last_res = 0;
-
-    // read ir sensors
-    // > threshold trigger , data = 1 
-    for(int i=0 ; i<IR_CNT ; i++){
-        ir_data[i] = analogRead(IR_PIN[i]) > IR_THRESHOLD[i];
-    }
-
-
-    #ifdef IR_DEBUG
-        for(int i=0 ; i<IR_CNT ; i++){
-            if(ir_data[i]){
-                Serial.print("1");
-            }else{
-                Serial.print("0");
+        ir_data[i] = analogRead(IR_PIN[i]);
+        if(update_range){
+            if(ir_data[i] > ir_max[i]){
+                ir_max[i] = ir_data[i];
+            }
+            if(ir_data[i] < ir_min[i]){
+                ir_min[i] = ir_data[i];
             }
         }
-        Serial.print("\t");
-    #endif
+    }
 
-    // find max continuous trigger ir sensors , then calculate the average position
-    int c_cnt = 0;
-    float c_sum = 0;
-    int max_cnt = 0;
-    float max_sum = 0;
-
+    // calculate error
+    ir_error = 0;
     for(int i=0 ; i<IR_CNT ; i++){
-        if(ir_data[i]){
-            c_cnt++;
-            c_sum += IR_WEIGHT[i];
-        }else{
-            c_cnt = 0;
-            c_sum = 0;
-        }
-        
-        if(c_cnt > max_cnt){
-            max_cnt = c_cnt;
-            max_sum = c_sum;
-        }
+        ir_error += float(ir_data[i] - ir_min[i]) / float(ir_max[i] - ir_min[i]) * IR_WEIGHT[i];
     }
-    float res;
-    if(max_cnt == 0){
-        if(last_res > 0){
-            res = (IR_CNT/2+0.5);
-        }
-        else if(last_res < 0){
-            res = -(IR_CNT/2+0.5);
-        }
-        else{
-            res = 0;
-        }
-    }else{
-        res = max_sum / max_cnt;
-        last_res = res;
-    }
-    
-    #ifdef IR_DEBUG
-        Serial.print(res);
-        Serial.print("\t");
-        Serial.println();
-    #endif
-    
-    return res;
-
+    return ir_error;
 }
+
+
+void ir_print_range(){
+    Serial.println("\t\t###IR Range###");
+    Serial.print("MAX:\t");for(int i=0 ; i<IR_CNT ; i++){Serial.print(ir_max[i]);Serial.print("\t");}
+    Serial.println();
+    Serial.print("MIN:\t");for(int i=0 ; i<IR_CNT ; i++){Serial.print(ir_min[i]);Serial.print("\t");}
+    Serial.println();
+}
+
+void ir_print_data(){
+    for(int i=0 ; i<IR_CNT ; i++){Serial.print(ir_data[i]);Serial.print("\t");}Serial.println();
+}
+
+void ir_print_error(){
+    Serial.print("IR Error:\t");Serial.println(ir_error);
+}
+
+
--- mbot_main/pid.h
+++ mbot_main/pid.h
@@ -1,23 +1,14 @@
+float Ltarget_speed = 130;
+float Rtarget_speed = 130;
 
-#define PID_V1
-
-
-
-float Ltarget_speed = 180;
-float Rtarget_speed = 180;
 int LMotorSpeed = 0;
 int RMotorSpeed = 0;
 
-
-#ifdef PID_V1
-
-
-#define kp 70 //80
+#define kp 100 //80
 #define ki 0
-#define kd 20
+#define kd 0
 
 void pid(float error , float dt){
-
     static float last_error = 0;
     static float integral = 0;
     float derivative;
@@ -29,55 +20,10 @@ void pid(float error , float dt){
     LMotorSpeed = Ltarget_speed + out;
     RMotorSpeed = Rtarget_speed - out;
 
-    if(LMotorSpeed<35){
-        LMotorSpeed -= 70;
-    }
-    if(RMotorSpeed<35){
-        RMotorSpeed -= 70;
-    }
-
-    last_error = error;
+    // if(LMotorSpeed<35){
+    //     LMotorSpeed -= 70;
+    // }
+    // if(RMotorSpeed<35){
+    //     RMotorSpeed -= 70;
+    // }
 }
-
-#endif
-
-
-
-
-#ifdef PID_V2
-
-#define kp 120 //80
-#define ki 0
-#define kd 50
-float Ltarget_speed = 255;
-float Rtarget_speed = 255;
-void pid(float error , float dt){
-
-    static float last_error = 0;
-    static float integral = 0;
-    float derivative;
-    
-    integral += error*dt;
-    derivative = (error-last_error)/dt;
-    int out = kp*error + ki*integral + kd*derivative;
-
-    // int left_speed = Ltarget_speed + out;
-    // int right_speed = Rtarget_speed - out;
-
-    int left_speed = Ltarget_speed;
-    int right_speed = Rtarget_speed;
-
-    if(out > 0){
-        right_speed -= out;
-    }
-    else if(out < 0){
-        left_speed += out;
-    }
-
-    LMotorSpeed = left_speed;
-    RMotorSpeed = right_speed;
-
-    last_error = error;
-}
-
-#endif
